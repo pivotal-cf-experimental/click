cccefd4d0c35407e1736b4ceb6d8385b429ec973
Mark Rushakoff <mark.rushakoff@gmail.com>
2013-12-03 07:32:27 -0800

    Add ObjectCount.by_session_name

--- lib/click/database/models/object_count.rb
+++ lib/click/database/models/object_count.rb
@@ -5,6 +5,14 @@ module Click
     module Models
       class ObjectCount < Sequel::Model
         many_to_one :snapshot
+
+        dataset_module do
+          def by_session_name(session_name)
+            session_dataset = Session.where(name: session_name)
+            snapshot_dataset = Snapshot.join(session_dataset, id: :session_id)
+            from_self.join(snapshot_dataset, id: :snapshot_id)
+          end
+        end
       end
     end
   end
--- spec/database/models/object_count_spec.rb
+++ spec/database/models/object_count_spec.rb
@@ -3,5 +3,24 @@ require 'click/database'
 
 initialize_models
 
-describe Click::Database::Models::ObjectCount do
+module Click::Database::Models
+  describe ObjectCount do
+    describe '.scopes' do
+      describe '.by_session_name' do
+        it 'returns only object counts belonging to the session with that name' do
+          ignored_session = Session.create(name: 'ignored', started_at: Time.now)
+          ignored_snapshot = Snapshot.create(timestamp: Time.now, session_id: ignored_session.id)
+          ObjectCount.create(class_name: "Foo", count: 1, snapshot_id: ignored_snapshot.id)
+
+          important_session = Session.create(name: 'important', started_at: Time.now)
+          important_snapshot = Snapshot.create(timestamp: Time.now, session_id: important_session.id)
+          important_object_count = ObjectCount.create(class_name: "Bar", count: 2, snapshot_id: important_snapshot.id)
+
+          dataset = ObjectCount.by_session_name('important')
+          expect(dataset.count).to eq(1)
+          expect(dataset.map([:id, :class_name, :count])).to eq([[important_object_count.id, "Bar", 2]])
+        end
+      end
+    end
+  end
 end
