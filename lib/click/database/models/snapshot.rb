a44282c25520f2f5a40aec069c38fe857ade3d99
Eric Malm <emalm@pivotallabs.com>
2013-12-04 14:44:51 -0800

    Add Snapshot#difference
    Signed-off-by: Mark Rushakoff <mrushakoff@pivotallabs.com>

--- lib/click/database/models/snapshot.rb
+++ lib/click/database/models/snapshot.rb
@@ -1,4 +1,5 @@
 require 'sequel'
+require 'set'
 
 module Click
   module Database
@@ -7,13 +8,27 @@ module Click
         many_to_one :session
         one_to_many :object_counts
 
+        def difference(earlier)
+          earlier_object_count_hash = Hash[earlier.object_counts_dataset.map([:class_name, :count])]
+          later_object_count_hash = Hash[object_counts_dataset.map([:class_name, :count])]
+
+          keys = Set.new(earlier_object_count_hash.keys + later_object_count_hash.keys)
+
+          diff = {}
+          keys.each do |key|
+            diff[key] = later_object_count_hash.fetch(key, 0) - earlier_object_count_hash.fetch(key, 0)
+          end
+
+          diff
+        end
+
         dataset_module do
           def after(time)
             filter { timestamp > time }
           end
 
           def before(time)
-            filter { timestamp < time}
+            filter { timestamp < time }
           end
 
           def by_session_name(session_name)
--- spec/database/models/snapshot_spec.rb
+++ spec/database/models/snapshot_spec.rb
@@ -85,5 +85,25 @@ module Click::Database::Models
         end
       end
     end
+
+    describe '#difference' do
+      it "returns a hash representing the differences between the snapshot's object counts" do
+        session = Session.create(name: "a session", started_at: Time.now - 100)
+
+        first_snapshot = Snapshot.create(timestamp: Time.now - 50, session_id: session.id)
+        first_snapshot.add_object_count(ObjectCount.new(class_name: "first_only", count: 10))
+        first_snapshot.add_object_count(ObjectCount.new(class_name: "both", count: 1))
+
+        second_snapshot = Snapshot.create(timestamp: Time.now, session_id: session.id)
+        second_snapshot.add_object_count(ObjectCount.new(class_name: "both", count: 5))
+        second_snapshot.add_object_count(ObjectCount.new(class_name: "second_only", count: 9))
+
+        expect(second_snapshot.difference(first_snapshot)).to eq(
+          "both" => 4,
+          "second_only" => 9,
+          "first_only" => -10,
+        )
+      end
+    end
   end
 end
