a4f0e0c16a1418458523e22f392a7048f0db46d3
Mark Rushakoff <mark.rushakoff@gmail.com>
2013-12-05 00:56:25 -0800

    Use rspec-given on clicker_spec

--- spec/clicker_spec.rb
+++ spec/clicker_spec.rb
@@ -1,99 +1,93 @@
 require 'spec_helper'
 
 describe Click::Clicker do
-  subject(:clicker) { described_class.new('test') }
+  Given(:session_name) { SecureRandom.uuid }
+  Given(:clicker) { described_class.new(session_name) }
 
   describe '#session_name' do
-    it 'is the string given to the constructor' do
-      expect(clicker.session_name).to eq('test')
-    end
+    Then { clicker.session_name == session_name }
   end
 
   describe '#instance_count' do
-    it 'counts instances of a single class' do
-      klass = Class.new
+    Given(:klass) { Class.new }
 
-      clicker.click!
-      expect(clicker.instance_count(klass)).to eq(0)
+    describe 'with no instances' do
+      When { clicker.click! }
 
-      obj = klass.new
-
-      clicker.click!
-      expect(clicker.instance_count(klass)).to eq(1)
-
-      obj = nil
-
-      clicker.click!
-      expect(clicker.instance_count(klass)).to eq(0)
+      Then { clicker.instance_count(klass) == 0 }
     end
 
-    it 'tracks differences in symbols' do
-      difference = 100
+    describe 'with instances' do
+      Given!(:object) { klass.new }
 
-      expect {
-        difference.times { |i| [Time.now.to_i, i].join('_-_-_').to_sym }
-      }.to change {
-        clicker.click!
-        clicker.instance_count(Symbol)
-      }.by_at_least(difference)
+      When { clicker.click! }
+
+      Then { clicker.instance_count(klass) == 1 }
     end
 
-    it 'counts BasicObjects as indeterminable' do
-      difference = 100
-      stuff = []
+    describe 'symbols' do
+      Given { clicker.click! }
+      Given!(:original_symbol_count) { clicker.instance_count(Symbol) }
 
-      expect {
-        difference.times { |i| stuff << BasicObject.new }
-      }.to change {
-        clicker.click!
-        clicker.instance_count(Click::Indeterminable)
-      }.by_at_least(difference)
+      When { 100.times { |i| [Time.now.to_i, i].join('_-_').to_sym }; clicker.click! }
+
+      Then { clicker.instance_count(Symbol) >= original_symbol_count + 100 }
     end
 
-    it 'counts objects that have overridden #class as indeterminable' do
-      difference = 100
-      stuff = []
-      klass = Class.new do
-        def class
-          nil
+    describe 'BasicObject' do
+      Given { clicker.click! }
+      Given!(:original_count) { clicker.instance_count(Click::Indeterminable) }
+
+      When { stuff = 25.times.map { BasicObject.new }; clicker.click! }
+
+      Then { clicker.instance_count(Click::Indeterminable) >= original_count + 25 }
+    end
+
+    describe 'objects that poorly override #class' do
+      # This test was flaking out with the Given setup. *shrug*
+      before do
+        klass = Class.new do
+          def class
+            nil
+          end
         end
+        clicker.click!
+        @original_count = clicker.instance_count(Click::Indeterminable)
+
+        @stuff = 33.times.map { klass.new }
       end
 
-      expect {
-        difference.times { |i| stuff << klass.new }
-      }.to change {
-        clicker.click!
-        clicker.instance_count(Click::Indeterminable)
-      }.by_at_least(difference)
+      When { clicker.click! }
+
+      Then { clicker.instance_count(Click::Indeterminable) >= @original_count + 33 }
     end
   end
 
   describe '#object_counts' do
-    it "groups objects by their class" do
-      ObjectSpace.should_receive(:each_object).and_yield(Object.new).and_yield(":)").and_yield(":(")
-      Symbol.should_receive(:all_symbols).and_return([:foo, :bar, :baz])
-
-      clicker.click!
-      expect(clicker.object_counts).to be_a(Hash)
-      expect(clicker.object_counts.to_a).to eq([[Object, 1], [String, 2], [Symbol, 3]])
+    Given do
+      ObjectSpace.stub(:each_object).and_yield(Object.new).and_yield(":)").and_yield(":(")
+      Symbol.stub(:all_symbols).and_return([:foo, :bar, :baz])
     end
+
+    When { clicker.click! }
+
+    Then { clicker.object_counts == {Object => 1, String => 2, Symbol => 3} }
+    And { expect(ObjectSpace).to have_received(:each_object).with(no_args) }
+    And { expect(Symbol).to have_received(:all_symbols).with(no_args) }
   end
 
   describe 'observers' do
-    it 'calls #on_add' do
-      observer = double(on_add: nil)
-      observer.should_receive(:on_add).with(clicker)
+    Given(:observer) { double(on_add: nil, before_click: nil, after_click: nil) }
 
-      clicker.add_observer(observer)
-    end
+    When { clicker.add_observer(observer) }
 
-    it 'calls #before_click and #after_click' do
-      observer = double(on_add: nil, before_click: nil, after_click: nil)
-      observer.should_receive(:before_click).with(clicker).ordered
-      observer.should_receive(:after_click).with(clicker).ordered
+    Then { expect(observer).to have_received(:on_add).with(clicker) }
 
-      clicker.add_observer(observer)
-      clicker.click!
+    describe 'after click!' do
+      When { clicker.click! }
+
+      Then { expect(observer).to have_received(:before_click).with(clicker) }
+      Then { expect(observer).to have_received(:after_click).with(clicker) }
     end
   end
 end
